fb4f8b22a504b80ad6e700e6c663a3c9e421324e
Luke Buchanan <luke1020will@gmail.com>
2025-01-20 20:38:50 -0500

    extensions

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... } :
+{ pkgs, config, ... } :
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -6,6 +6,28 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  # Extensions
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+	blur-my-shell
+	burn-my-windows
+  ];
+
+  #dconf = {
+  #      enable = true;
+  #      settings = {
+  #      	"org/gnome/shell" = {
+  #      		disable-user-extensions = false;
+  #      		enabled-extensions = [
+  #      			pkgs.gnomeExtensions.blur-my-shell.extensionUuid
+  #      		];
+  #      	};
+  #      	"org/gnome/mutter" = {
+  #      		experimental-features = "['scale-monitor-framebuffer']";
+  #      	};
+
+  #      };
+  #};
 
   # Configure keymap in X11
   services.xserver.xkb = {
